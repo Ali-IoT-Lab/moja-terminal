e1f6ceddcbebe30e8b4472e0edc26eff812ee66d
Legendary <156110386@qq.com>
2018-11-01 23:03:54 +0800

    Update set_key.sh

--- scripts/set_key.sh
+++ scripts/set_key.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-option=$1
-moja_key=$2
+moja_key=$1
 HOME_DIR=""
 touch ~/.moja
 osType=`uname -s|tr '[A-Z]' '[a-z]'`
@@ -15,13 +14,11 @@ fi
 
 hostName=`cat /$HOME_DIR/moja/.moja/moja-cloud-server-host|tr -d '\n'`
 curl -o ./$moja_key $hostName/shells/$moja_key
-publickey=`cat ./$moja_key |grep "publicKey="|awk '=' {print $2}`
-email=`cat ./$moja_key |grep "email="|awk '=' {print $2}``
 
-publicKey=echo -n "str" | base64 -d
-echo "module.exports ={publicKey:\\\``+publicKey+`\\\`}" > /$HOME_DIR/moja/.moja/publicKey.js
-echo "module.exports ={email:\\\``+email+`\\\`}" > /$HOME_DIR/moja/.moja/email.js
+publickey=`cat ./$moja_key |grep "publicKey="|awk -F '"' '{print $2}'`
 
-if [ $option = "-k" ] ; then
-  echo "module.exports ={user_key:$moja_key}" > ~/.moja
-fi
+email=`cat ./$moja_key |grep "email="|awk -F '"' '{print $2}'`
+publicKey=`echo -n $publickey| base64 -d`
+echo "module.exports ={publicKey:\`$publicKey\`}" > /$HOME_DIR/moja/.moja/publicKey.js
+echo "module.exports ={email:\`$email\`}" > /$HOME_DIR/moja/.moja/email.js
+echo "module.exports ={user_key:$moja_key}" > ~/.moja
