d7757ca6d6f13ef09a9cd2146b700ac519de1166
Legendary <156110386@qq.com>
2018-11-01 22:34:57 +0800

    Update moja_pos.sh

--- scripts/moja_pos.sh
+++ scripts/moja_pos.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
 
-osType=`uname -s|tr '[A-Z]' '[a-z]'`
-
-if [ $osType = "linux" ] ;then
-  mv /home/moja/.moja/lib/node_modules/moja-terminal /home/moja/.moja/
-elif [ $osType = "darwin" ] ;then
-  mv /Users/moja/.moja/lib/node_modules/moja-terminal /Users/moja/.moja/
-else
-  echo "不支持的系统类型！"
-fi
