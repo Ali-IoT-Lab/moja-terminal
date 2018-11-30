2e710bdb0a4131f474a87aec74269485dda467e0
忙僧2018 <156110386@qq.com>
2018-11-30 11:12:44 +0800

    node-pty-prebuilt安装过程小概率报错gcc+notfound

---
+++ scripts/preinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+g++ -v
+if [ $? -ne 0 ] ; then
+  if command_exists yum ; then
+    yum install gcc-c++ -y
+  else
+    apt-get install gcc-c++ -y
+  fi
+fi
