91436329da65ee91834b348c63ea09bb89de5565
忙僧2018 <156110386@qq.com>
2019-01-10 01:21:57 +0800

    提示创建moja目录全局

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,6 +3,6 @@
 if ! moja
 then
   path=`npm |grep npm@| awk -F ' ' '{print $2}'|awk -F '/npm' '{print $1}'`;
-  mojaPath= $path/moja-terminal/bin/moja
+  mojaPath= "$path/moja-terminal/bin/moja"
   echo "请手动执行======>>>>>：sudo ln -s $mojaPath /usr/bin/moja"
 fi
