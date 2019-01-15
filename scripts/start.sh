00ab57a19a9d1699e0e8dbe97f00a345e9ff4907
忙僧2018 <156110386@qq.com>
2019-01-15 11:32:01 +0800

    添加id文件

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,21 +12,31 @@ publickey=`cat ~/$moja_key |grep "publicKey="|awk -F '"' '{print $2}'`
 email=`cat ~/$moja_key |grep "email="|awk -F '"' '{print $2}'`
 clientVersion=`cat ~/$moja_key |grep "clientVersion="|awk -F '"' '{print $2}'`
 
-touch -p $moja_home/publicKey.js
+mkdir -p $moja_home
+touch $moja_home/publicKey.js
 touch $moja_home/email.js
 touch $moja_home/moja-cloud-server-host
 touch $moja_home/stage
 touch $moja_home/moja-version
 mkdir /var/tmp/client-logs
+mkdir ~/.moja/pmtwo
+mkdir ~/.moja/client
 
 echo $clientVersion > $moja_home/moja-version
 echo "module.exports ={publicKey:\`$publickey\`}" > $moja_home/publicKey.js
 echo "module.exports ={email:\`$email\`}" > $moja_home/email.js
 
-mkdir ~/.moja/pmtwo
-mkdir ~/.moja/client
+if [ ! -f "$moja_home/terminalId.js" ]; then
+   touch $moja_home/terminalId.js
+   echo "module.exports =\"\";" > $moja_home/terminalId.js
+fi
+if [ ! -f "$moja_home/userId.js" ]; then
+  touch $moja_home/userId.js
+  echo "module.exports =\"\";" > $moja_home/userId.js
+fi
 
 #第二步 下载客户单代码
+npm config set loglevel=http
 npm install remote-terminal-client-test --unsafe-perm=true --registry=https://registry.cnpmjs.org --prefix ~/.moja/client
 #第三步 安装pm2
 npm install pm2 --unsafe-perm=true --registry=https://registry.cnpmjs.org --prefix ~/.moja/pmtwo
