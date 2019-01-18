e03430092bdd043e3b510d7cfdc86b71f69be844
忙僧2018 <156110386@qq.com>
2019-01-18 19:16:21 +0800

    ubuntu安装

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-findNpm="\`npm |grep npm@| awk -F ' ' '{print \$2}'|awk -F '/npm' '{print \$1}'\`"
+findNpm="npm |grep npm@| awk -F ' ' '{print \$2}'|awk -F '/npm' '{print \$1}'"
 command_exists() {
 	command -v "$@" > /dev/null 2>&1
 }
 
 if ! command_exists npm ; then
-  echo -e "\033[31m 请手动执行：sudo ln -s $findNpm/moja-terminal/bin/moja /usr/bin/moja \033[0m"
+  echo -e "\033[31m 请手动执行：sudo ln -s \`$findNpm\`/moja-terminal/bin/moja /usr/bin/moja \033[0m"
   exit 0
 fi
 
